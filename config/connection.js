b31d54e7842fa0a51beecffef8fe5fa62445240b
njacques47 <nojacques94@gmail.com>
2022-05-16 01:36:12 -0400

    created db connection and environment variables in env file

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+// import the sequelize constructor from the library
+const Sequelize = require('sequelize');
+
+// create connection to our database, pass in your mysql info for username/password
+const sequelize = new Sequelize('process.env.DB_NAME', 'process.env.DB_USER', 'process.env.DB_PASSWORD', {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
+});
+
+module.exports = sequelize;
