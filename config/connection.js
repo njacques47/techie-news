91d6c9447cb21bddd9bd719ed5f8a95716d511ba
njacques47 <nojacques94@gmail.com>
2022-05-19 22:41:08 -0400

    fixed server error

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,10 @@
 // import the sequelize constructor from the library
 const Sequelize = require('sequelize');
 
+require('dotenv').config();
+
 // create connection to our database, pass in your mysql info for username/password
-const sequelize = new Sequelize('process.env.DB_NAME', 'process.env.DB_USER', 'process.env.DB_PASSWORD', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: 'localhost',
   dialect: 'mysql',
   port: 3306
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
+// const path = require('path');
+// app.use(express.static(path.join(__dirname, 'public')));
 
 const express = require('express');
 const routes = require('./routes');
