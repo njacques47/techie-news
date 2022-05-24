6ee730e6f31b76578d12acd97742a0cab31882b0
njacques47 <nojacques94@gmail.com>
2022-05-23 20:17:54 -0400

    defined model associations

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations
+User.hasMany(Post, { // saying that a user can make many posts
+  foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, { // a single post can only have one author
+  foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
+// module 13.3.5
